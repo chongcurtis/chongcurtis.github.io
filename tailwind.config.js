4940adbfb09ae5b47218d4b43e37650162e6e8fd
Curtis Chong <curtischong5@gmail.com>
2023-04-13 22:24:27 -0400

    more setup cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
+
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        // color names found via this website: https://www.color-name.com/hex/ff947d
+        "light-salmon": "#FF947D",
+        "maya-blue": "#66CCFF",
+      },
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
+    ...defaultTheme,
   },
   plugins: [],
 }
