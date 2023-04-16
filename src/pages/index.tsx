9f5f47c44e1d436a50a83b139098b9bd9de0c9ad
Curtis Chong <curtischong5@gmail.com>
2023-04-16 02:11:10 -0400

    working on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
+import Link from "next/link";
+
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24 back bg-background-color">
+        <main className="back flex min-h-screen flex-col items-center justify-between bg-background-color p-24">
             {/*
             sleepovers
             microbogs
@@ -10,11 +12,21 @@ export default function Home() {
             */}
             <div className="place-items-center ">
                 <p>
-                    Hello! I&apos;m Curtis. I guess for intros I grew up in the GTA and moved to
-                    Waterloo for school. I&apos;m currently trying to start a company with some
-                    friends. Anyway, This site is my yellow brick road in progress. Wanna look
-                    around?
+                    Hello I&apos;m Curtis! I grew up in the GTA and moved to Waterloo for school.
+                    I&apos;m currently trying to build a zero-knowledge proofs company with some
+                    friends. Anyhow, I made this site to document my yellow brick road in progress.
+                    Wanna look around?
                 </p>
+            </div>
+            <div>
+                {/*<Link href="/sleepovers" className="border-b-sleepover-secondary border-b-2">*/}
+                <Link
+                    href="/sleepovers"
+                    className="underline decoration-sleepover-secondary underline-offset-2 hover:decoration-wavy"
+                >
+                    My newsletter about project ideas
+                </Link>
+                <p>Projects I'm proud of</p>
             </div>
         </main>
     );
