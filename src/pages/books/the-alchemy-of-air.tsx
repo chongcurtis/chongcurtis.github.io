5b1487dfee99c6cbbfd476743dcfd99fc03c08d4
Curtis Chong <curtischong5@gmail.com>
2023-05-14 13:39:08 -0400

    added gpt4 collision simulator

--- src/pages/books/the-alchemy-of-air.tsx
+++ src/pages/books/the-alchemy-of-air.tsx
@@ -1,8 +1,6 @@
-import Boiler from "@/pages/books/the-alchemy-of-air/Boiler";
 import React from "react";
 import { initAnimations } from "@/common/animations";
-import AlchemyOfAirTitle from "@/pages/books/the-alchemy-of-air/AlchemyOfAirTitle";
-import BulletsStrikeSand from "@/pages/books/the-alchemy-of-air/BulletsStrikeSand";
+import Boiler from "@/pages/books/the-alchemy-of-air/Boiler";
 
 export default function TheAlchemyOfAir() {
     React.useEffect(() => {
@@ -14,16 +12,30 @@ export default function TheAlchemyOfAir() {
             <div className="mt-20" />
             {/*<AlchemyOfAirTitle />*/}
             {/*TODO: uncomment after fixing collision*/}
-            {/*<Boiler />*/}
-            <BulletsStrikeSand />
+            <Boiler />
+            {/*<BulletsStrikeSand />*/}
             {/*</div>*/}
+            <p className="fade-in-on-scroll text-xl">
+                It's 1890 when Crookes pulled the fire alarm.
+            </p>
+            <br />
+            <p className="fade-in-on-scroll text-xl">
+                "England and all civilized nations, stand in peril."
+            </p>
+            <br />
+            <p className="fade-in-on-scroll text-xl">"As mouths multiply, food sources dwindle"</p>
+            <p>"There was only one answer: The creation of vast amount of fertilizer"</p>
+            <p>"It is the chemist who must come to the rescue"</p>
+            <p>Enter fritz Haber. A German, and a chemist still looking for his Nobel prize.</p>
+            <p>He labours </p>
             <p className="fade-in-on-scroll">
                 This book changed my life because it taught me that humanity has solved
-                calamity-scale problems before: Running out of food. It's about how a scientist and
-                an engineer spent years bringing down the cost of man-made fertilizer to replace the
-                world's limitted supply of natural fertilizer, feeding half the world population. It
-                made me much more optimistic about climate change. It was really fascinating because
-                these people solved the world's biggest problem and nobody knows about it.
+                calamity-scale problems before. It make me much more optimistic in technology that
+                can save the world. We've done it before.
+            </p>
+            <p>
+                What else did these Nobel laureates do? More alchemy of course. You can feel it in
+                the air.
             </p>
             <div className="mb-40" />;
         </>
---
+++ src/pages/books/the-alchemy-of-air/collision-simulator/Circle.ts
@@ -0,0 +1,15 @@
+import Vector from "@/pages/books/the-alchemy-of-air/collision-simulator/Vector";
+
+export default class Circle {
+    constructor(public center: Vector, public radius: number, public velocity: Vector) {}
+
+    containsPoint(point: Vector): boolean {
+        const difference = this.center.subtract(point);
+        return difference.length() <= this.radius;
+    }
+
+    // Call this function every frame to move the circle
+    updatePosition(timeStep: number): void {
+        this.center = this.center.add(this.velocity.scale(timeStep));
+    }
+}
