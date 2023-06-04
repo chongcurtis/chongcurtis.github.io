a5fb8fe97b00fb1dfe5bd5283b98427a16541af3
Curtis Chong <curtischong5@gmail.com>
2023-06-04 00:38:16 -0400

    touched up some styling for epicness

--- src/pages/books/the-alchemy-of-air.tsx
+++ src/pages/books/the-alchemy-of-air.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { initAnimations } from "@/common/animations";
 import ExponentialCount from "@/pages/books/the-alchemy-of-air/ExponentialCount";
+import AlchemyOfAirTitle from "@/pages/books/the-alchemy-of-air/AlchemyOfAirTitle";
+import sirWilliamCrookes from "public/books/the-alchemy-of-air/Sir_William_Crookes_1906.jpg";
+import FixNitrogen from "@/pages/books/the-alchemy-of-air/FixNitrogen";
 
 export default function TheAlchemyOfAir() {
     React.useEffect(() => {
@@ -10,7 +13,7 @@ export default function TheAlchemyOfAir() {
     return (
         <>
             <div className="mt-20" />
-            {/*<AlchemyOfAirTitle />*/}
+            <AlchemyOfAirTitle />
             <p className="fade-in-on-scroll animation-delay-2000 text-md mt-20">
                 Credit for this animation goes to{" "}
                 <a
@@ -29,11 +32,11 @@ export default function TheAlchemyOfAir() {
                         enough to eat."
                     </p>
                 </div>
-                {/*<img*/}
-                {/*    className="fade-in-on-scroll m-5 max-w-sm p-20"*/}
-                {/*    src={sirWilliamCrookes.src}*/}
-                {/*    alt="Sir William Crookes"*/}
-                {/*/>*/}
+                <img
+                    className="fade-in-on-scroll m-5 max-w-sm p-20"
+                    src={sirWilliamCrookes.src}
+                    alt="Sir William Crookes"
+                />
             </div>
             <p className="fade-in-on-scroll mt-10 text-lg">
                 All 1000 people in the auditorium muted to hear the 66-year-old Crookes commence his
@@ -44,108 +47,79 @@ export default function TheAlchemyOfAir() {
                 say will prove somewhat dull."
             </p>
             {/*https://www.jstor.org/stable/1627238?seq=2*/}
-            {/*<p className="fade-in-on-scroll mt-10 text-xl">*/}
-            {/*    "As mouths multiply, food sources dwindle"*/}
-            {/*</p>*/}
-            {/*<p className="fade-in-on-scroll mt-10 text-xl">*/}
-            {/*    At present, England's fertilizer is imported from the Chilian desert. But it will*/}
-            {/*    soon run out.*/}
-            {/*</p>*/}
-            <p className="fade-in-on-scroll mt-10 text-xl">
+            <p className="fade-in-on-scroll mt-10 text-lg">
                 Even if all arable land is cultivated, and crops are optimally rotated, and everyone
                 becomes vegetarian...
             </p>
-            <p className="fade-in-on-scroll mt-10 text-xl">
+            <p className="fade-in-on-scroll mt-10 text-lg">
                 "Our wheat-producing soil is totally unequal to the strain put upon it."
             </p>
-            <p className="fade-in-on-scroll mt-10 text-xl">
+            <p className="fade-in-on-scroll mb-20 mt-10 text-lg">
                 {" "}
                 So England will have to import. However...
             </p>
-            <p className="fade-in-on-scroll mt-[100px] text-xl">
+            <p className="fade-in-on-scroll mb-10 text-lg">
                 Austro-Hungary will not have surplus wheat{" "}
             </p>
-            <p className="fade-in-on-scroll mt-10 text-xl"> Canada will not have surplus wheat</p>
-            <p className="fade-in-on-scroll mt-10 text-xl"> Russia will not have surplus wheat</p>
-            <p className="fade-in-on-scroll mt-10 text-xl"> America will not have surplus wheat</p>
-            <p className="fade-in-on-scroll mt-10 text-xl">And if there's a war, imports will...</p>
-            <p className="fade-in-on-scroll mb-[150px] mt-[150px] text-xl">
+            <p className="fade-in-on-scroll mb-10 text-lg"> Canada will not have surplus wheat</p>
+            <p className="fade-in-on-scroll mb-10 text-lg"> Russia will not have surplus wheat</p>
+            <p className="fade-in-on-scroll mb-10 text-lg"> America will not have surplus wheat</p>
+            <p className="fade-in-on-scroll mb-10 text-lg">And if there's a war, imports will...</p>
+            <p className="fade-in-on-scroll mb-20 mt-20 text-lg">
                 England will{" "}
                 <span className="underline-on-scroll animation-delay-200 after:bg-red-400">
                     starve
                 </span>{" "}
                 in 33 years.
             </p>
-            <p className="fade-in-on-scroll mt-10 text-xl">Tense silence suffocates the room</p>
-            <p className="fade-in-on-scroll mt-10 text-xl">
+            <p className="fade-in-on-scroll mt-10 text-lg">Tense silence suffocates the room</p>
+            <p className="fade-in-on-scroll mt-10 text-lg">
                 "Wheat preeminently demands nitrogen, fixed in the form of ammonia"
             </p>
-            <p className="fade-in-on-scroll mt-10 text-xl">
+            <p className="fade-in-on-scroll mt-10 text-lg">
                 "We must then rely on nitrogenous manures to increase the fertility of land under
                 wheat, so as to raise the yield from the world's low average - 12.7 bushels per acre
                 - to a higher average".
             </p>
-            <p className="fade-in-on-scroll mt-10 text-xl">
+            <p className="fade-in-on-scroll mt-10 text-lg">
                 Yet "for years we have been expending fixed nitrogen at a culpably extravagant
                 rate".
             </p>
-            <p className="fade-in-on-scroll mt-10 text-xl">
+            <p className="fade-in-on-scroll mt-10 text-lg">
                 At present, England's fertilizer is imported from the Chilean desert. But it will
                 soon empty.
             </p>
-            <p className="fade-in-on-scroll mt-10 text-xl">
+            <p className="fade-in-on-scroll mt-10 text-lg">
                 Fortunately, "nitrogen is one of the most abundant and pervading bodies on the face
                 of the earth".
             </p>
-            {/*<p className="fade-in-on-scroll mt-10 text-xl">It's 78% of the air.</p>*/}
-            <p className="fade-in-on-scroll mt-10 text-xl">
+            <p className="fade-in-on-scroll mt-10 text-lg">
                 But to turn N&#8322; into fertilizer, you must break nature's strongest molecular
                 bond.
             </p>
-            <p className="fade-in-on-scroll mb-[100px] mt-[100px] text-xl">
+            <FixNitrogen />
+            <p className="fade-in-on-scroll mb-20 mt-10 text-lg">
                 And it has never been accomplished.
             </p>
-            {/*<FixNitrogen />*/}
-            {/*<p className="fade-in-on-scroll mt-10 text-xl">*/}
-            {/*    "For years we have been expending fixed nitrogen at a culpably extravagant rate,*/}
-            {/*    heedless of the fact that it is [naturally] fixed with extreme slowness and*/}
-            {/*    difficulty".*/}
-            {/*</p>*/}
-            {/*<p className="fade-in-on-scroll mt-10 text-xl">*/}
-            {/*    "There is a gleam of light out of this darkness of despondency. In its free state,*/}
-            {/*    nitrogen is one of the most abundant and pervading bodies on the face of the earth".*/}
-            {/*</p>*/}
-            {/*<p className="fade-in-on-scroll mt-10 text-xl">Brook ends on a hopeful note:</p>*/}
-            {/*<p className="fade-in-on-scroll mt-10 text-xl">*/}
-            {/*    "The fixation of nitrogen is vital to the progress of civilized humanity".*/}
-            {/*</p>*/}
-            <p className="fade-in-on-scroll mt-10 text-xl">
+            <p className="fade-in-on-scroll mt-10 text-lg">
                 "There is a gleam of light out of this darkness of despondency. [...] Before we are
                 in the grip of actual dearth, the chemist will step in and postpone the day of
                 famine".
             </p>
-            {/*<p className="fade-in-on-scroll mt-10 text-xl">*/}
-            {/*    Many didn't believe him, stating that the Chilian desert has an unlimitted amount of*/}
-            {/*    fertilizer.*/}
-            {/*</p>*/}
-            {/*<p className="fade-in-on-scroll mt-10 text-xl">*/}
-            {/*    But they were wrong. Because our population doubled, and doubled, and doubled.*/}
-            {/*</p>*/}
-            <p className="fade-in-on-scroll mt-10 text-xl">
+            <p className="fade-in-on-scroll mt-10 text-lg">
                 This was Brook's challenge: To fix nitrogen, and save mankind.
             </p>
-            <p className="fade-in-on-scroll mt-10 text-xl">
+            <p className="fade-in-on-scroll mt-10 text-lg">
                 His address was sensational. Newspapers printed it around the world.
             </p>
             <p className="fade-in-on-scroll mt-40 text-2xl">The race to fix nitrogen had begun.</p>
-            {/* <p className="fade-in-on-scroll mt-40 text-xl">31 years until famine</p> */}
-            <div className="fade-in-on-scroll mt-40 flex flex-row justify-center text-xl">
+            <div className="fade-in-on-scroll mb-60 mt-40 flex flex-row justify-center text-lg">
                 <ExponentialCount startingNumber={100} endingNumber={31} exponentialAmount={0.95} />
                 <p>Years until Famine </p>
             </div>
-            <p className="fade-in-on-scroll mt-20 text-2xl">1895 Germany</p>
-            <p className="fade-in-on-scroll mt-40 text-xl">Enter Fritz Haber</p>
-            <p className="fade-in-on-scroll mt-40 text-xl">20 years until famine</p>
+            <p className="fade-in-on-scroll text-2xl">1895 Germany</p>
+            <p className="fade-in-on-scroll mt-40 text-lg">Enter Fritz Haber</p>
+            <p className="fade-in-on-scroll mt-40 text-lg">20 years until famine</p>
             {/*<Boiler />*/}
             {/*<BulletsStrikeSand />*/}
             {/*</div>*/}
