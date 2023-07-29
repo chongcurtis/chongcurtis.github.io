f9482446833b79110ec166c028ff33766258471b
Curtis Chong <curtischong5@gmail.com>
2023-07-29 13:08:57 -0400

    add deployment config to main (#9)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,24 @@
+// https://www.viget.com/articles/host-build-and-deploy-next-js-projects-on-github-pages/
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+    assetPrefix = `/${repo}/`;
+    basePath = `/${repo}`;
+}
+
 /** @type {import("next").NextConfig} */
 const nextConfig = {
+    // assetPrefix: assetPrefix,
+    // basePath: basePath,
+    // images: {
+    //     loader: "imgix",
+    //     path: 'the "domain" of your Imigix source',
+    // },
     reactStrictMode: true,
 };
 
