911b1597e5fc14184bbe8743b80ed1263d117012
Curtis Chong <curtischong5@gmail.com>
2023-08-06 09:35:07 -0400

    fix metatag location

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Poppins } from "next/font/google";
 import type { AppProps } from "next/app";
 import Sidebar from "@/components/sidebar/Sidebar";
 import classNames from "classnames";
+import Head from "next/head";
 
 // how to serve fonts: https://nextjs.org/docs/basic-features/font-optimization
 const poppins = Poppins({
@@ -14,10 +15,17 @@ const poppins = Poppins({
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <main className={classNames(poppins.className)}>
-            <Sidebar>
-                <Component {...pageProps} />
-            </Sidebar>
-        </main>
+        <>
+            <Head>
+                <title>Curtis</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+                {/* <meta name="viewport" content="initial-scale=1, maximum-scale=1" /> */}
+            </Head>
+            <main className={classNames(poppins.className)}>
+                <Sidebar>
+                    <Component {...pageProps} />
+                </Sidebar>
+            </main>
+        </>
     );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html lang="en" className="bg-background-color">
-            <Head>
-                <title>Curtis</title>
-                {/* <meta name="viewport" content="width=device-width, initial-scale=1"></meta> */}
-                <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
-            </Head>
+            <Head />
             <body>
                 <Main />
                 <NextScript />
