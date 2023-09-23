9e073274f9568d83bc566571ddec0c4bce8f12a9
Curtis Chong <curtischong5@gmail.com>
2023-09-23 01:36:38 -0400

    switched font to outfit (#16)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
 import "@/styles/globals.css";
 
-import { Poppins } from "next/font/google";
+import { Outfit } from "next/font/google";
 import type { AppProps } from "next/app";
 import Sidebar from "@/components/sidebar/Sidebar";
 import classNames from "classnames";
 import Head from "next/head";
 
 // how to serve fonts: https://nextjs.org/docs/basic-features/font-optimization
-const poppins = Poppins({
+const poppins = Outfit({
     weight: ["100", "300", "700"],
     style: "normal",
-    subsets: ["latin-ext"],
+    subsets: ["latin"],
 });
 
 export default function App({ Component, pageProps }: AppProps) {
