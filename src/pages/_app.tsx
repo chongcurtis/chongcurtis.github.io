5e51a71f81e02e8f224efe89a0871a84a8db652e
Curtis Chong <curtischong5@gmail.com>
2023-11-26 13:46:35 -0500

    add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import type { AppProps } from "next/app";
 import Sidebar from "@/components/sidebar/Sidebar";
 import classNames from "classnames";
 import Head from "next/head";
+import Script from "next/script";
 
 // how to serve fonts: https://nextjs.org/docs/basic-features/font-optimization
 const poppins = Outfit({
@@ -21,6 +22,17 @@ export default function App({ Component, pageProps }: AppProps) {
                 <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
                 {/* <meta name="viewport" content="initial-scale=1, maximum-scale=1" /> */}
             </Head>
+            <Script
+                strategy="lazyOnload"
+                src="https://www.googletagmanager.com/gtag/js?id=G-TCKVPGLF3X"
+            ></Script>
+            <Script strategy="lazyOnload" id="analytics">
+                {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TCKVPGLF3X');`}
+            </Script>
             <main className={classNames(poppins.className)}>
                 <Sidebar>
                     <Component {...pageProps} />
