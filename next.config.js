bc690e1070c9fa870e4a0565287712168cee9951
Curtis Chong <curtischong5@gmail.com>
2024-11-20 15:13:15 -0800

    Firebase hosting (#23)

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
-// https://www.viget.com/articles/host-build-and-deploy-next-js-projects-on-github-pages/
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "/";
-
-if (isGithubActions) {
-    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-    assetPrefix = `/${repo}/`;
-    basePath = `/${repo}`;
-}
-
 /** @type {import("next").NextConfig} */
 const nextConfig = {
     // assetPrefix: assetPrefix,
@@ -20,6 +7,7 @@ const nextConfig = {
     //     path: 'the "domain" of your Imigix source',
     // },
     reactStrictMode: true,
+    output: "export",
     images: {
         unoptimized: true,
     },
