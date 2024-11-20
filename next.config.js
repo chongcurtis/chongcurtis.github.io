88a3b7cce068e2de9365ff093c38a2a1f5f5aabf
Curtis Chong <curtischong5@gmail.com>
2024-11-20 15:10:58 -0800

    rm github pages actionss

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
-// https://www.viget.com/articles/host-build-and-deploy-next-js-projects-on-github-pages/
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "/";
-
-if (isGithubActions) {
-    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-    assetPrefix = `/${repo}/`;
-    basePath = `/${repo}`;
-}
-
 /** @type {import("next").NextConfig} */
 const nextConfig = {
     // assetPrefix: assetPrefix,
